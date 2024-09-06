b44ac32eeedecb7fa84553ce0ed7a511b648c4b5
Wenhe Wang <mr.lasky@icloud.com>
2024-09-06 16:33:34 +0900

    update in progress

--- aya_n_body_ultimate_calculator_pack/n_body_class.py
+++ aya_n_body_ultimate_calculator_pack/n_body_class.py
@@ -1,8 +1,9 @@
-from baby_math import *
+from .baby_math import *
 import os
-import random
+import random as rand
 import vpython
 from vpython import *
+
 
 class n_body_system():
     def __init__(self, n):
@@ -10,40 +11,56 @@ class n_body_system():
         self.G = 6.6743e-11 # [m^3/(kg^1*s^2)]
 
         ##### Initial conditions
-        self.mass_order_of_magnitude = 10
-        self.distance_order_of_magnitude = 30
+        self.mass_order_of_magnitude = 30
+        self.distance_order_of_magnitude = 10
         self.velocity_order_of_magnitude = 4
         self.p_0 = None
         self.dpdt_0 = None
-        self.mass_vecter = None
-
+        self.mass_vector = None
+        self.initial_condition_is_set_flag = False
         ##### Sky box size
         self.sky_box_size = 10 * self.distance_order_of_magnitude
 
+        ##### Max simulation steps
+        self.kill_counter = 0
+        self.max_simulation_count = 100
+
+    def kill_the_kernel_countdown(self):
+        self.kill_counter +=1
+        if self.kill_counter > self.max_simulation_count:
+            print('max_simulation_count is reached, exiting...')
+            os._exit(0)
+        else:
+            print(f'Current simulation step: [{self.kill_counter}/{self.max_simulation_count}]')
+
     def generate_initial_conditions(self):
         ##### Random mass vector
-        self.mass_vecter = np.array([self.n,1])
-        for i in range(len(self.n)):
-            self.mass_vecter[i] = random.ranint() * self.mass_order_of_magnitude
+        self.mass_vector = np.zeros([self.n,1])
+        for i in range(self.n):
+            self.mass_vector[i] = rand.randint(1,9) * 10 ** self.mass_order_of_magnitude
         ##### Random initial position
-        self.p_0 = np.array([3 * self.n,1])
-        for i in range(len(self.n)):
-            self.p_0[i] = (2 * random.random() - 1.0) * self.distance_order_of_magnitude
+        self.p_0 = np.zeros([3 * self.n,1])
+        for i in range(self.n):
+            for k in range(3):
+                self.p_0[3*i + k] = (2 * rand.random() - 1.0) * 10 **  self.distance_order_of_magnitude
         ##### Random initial velocity
-        self.dpdt_0 = np.array([3 * self.n,1])
-        for i in range(len(self.n)):
-            self.dpdt_0[i] = (2 * random.random() - 1.0) * self.velocity_order_of_magnitude
+        self.dpdt_0 = np.zeros([3 * self.n,1])
+        for i in range(self.n):
+            for k in range(3):
+                self.dpdt_0[3*i + k] = (2 * rand.random() - 1.0) * 10 ** self.velocity_order_of_magnitude
+        self.initial_condition_is_set_flag = True
 
-    def engage_vpython_visualization(self, dt=10, simulation_steps=200):
+    def engage_vpython_visualization(self, dt=10, endless_simulation=False):
         ##### Break condition
-        if self.p_0 == None:
+        if self.initial_condition_is_set_flag == False:
             print('Initial conditions are not defined, exiting')
             os._exit(0)
 
         ##### Log the solutions to last_xxxx
-        self.last_p_0 = None
-        self.last_dpdt_0 = None
-        self.last_mass_vecter = None
+        self.time_stamp = 0.0
+        p_k = None
+        dpdt_k = None
+
 
         ###### Initialize vpython bodies
         self.vpython_body_list = []
@@ -51,17 +68,48 @@ class n_body_system():
             initialize_body = vpython.sphere(color = color.green, radius = 0.3, make_trail = True, retain=40)
             self.vpython_body_list.append(initialize_body)
 
-        # Simulation and visualization
+        ###### Starting the simulation
+        p_k = self.p_0 
+        dpdt_k = self.dpdt_0 
+        print(f'Starting the simulation, dt = {dt}')
         while(1):
-            # 1. Calculate 1~100 step
-            # 1.5 Handle the solution p_i = [x_i, y_i, z_i], dpdt_i = [dxdt_i, dydt_i, dzdt_i]
-            # 2. Update the visualization
-            # 3. Check if all the body is out of bound, teleportations
-            # 4. Log the solutions as initial conditions
-            pass
+            if endless_simulation == False:
+                self.kill_the_kernel_countdown()
+            ###### 1. Calculate 1~100 step
+            for i in range(100):
+                # print(p_k)
+                p_k_plus_1, dpdt_k_plus_1 = euler_s_method_one_step(dt, p_k, dpdt_k, self.G, self.mass_vector)
+                p_k = p_k_plus_1
+                dpdt_k = dpdt_k_plus_1
+            ###### 1.5 Handle the solution p_i = [x_i, y_i, z_i], dpdt_i = [dxdt_i, dydt_i, dzdt_i]
+            p_solution_list_k_plus_1 = [] # p_solution_list = [p_1, p_2, ... , p_n]
+            dpdt_solution_list_k_plus_1 = []
+            for i in range(self.n):
+                p_i = np.zeros([3,1])
+                dpdt_i = np.zeros([3,1])
+                for k in range(3):
+                    p_i[k] = p_k[3*i + k]
+                    dpdt_i[k] = dpdt_k[3*i + k]
+                p_solution_list_k_plus_1.append(p_i / (10**self.distance_order_of_magnitude))
+                # dpdt_solution_list_k_plus_1.append(dpdt_i / self.velocity_order_of_magnitude)
+            ###### 2. Update the visualization
+            rate(60)
+            # print(p_solution_list_k_plus_1)
+            for i in range(self.n):
+                self.vpython_body_list[i].pos = vector(p_solution_list_k_plus_1[i][0], p_solution_list_k_plus_1[i][1], p_solution_list_k_plus_1[i][2])
+            ###### 3. Check if all the body is out of bound, teleportations
+            ###### 4. Log the solutions as initial conditions
 
 def euler_s_method_one_step(dt, initial_p, initial_dpdt, G, mass_vector):
-    pass
+    ##### p_k
+    p_k = initial_p
+    dpdt_k = initial_dpdt
+    ##### F_dir at time k
+    F_dir_matrix_k = update_gravtational_interaction_matrix(p_k) 
+    ##### Euler's method for k+1
+    p_k_plus_1 = p_k + dt * dpdt_k
+    dpdt_k_plus_1 = dpdt_k + dt * G * F_dir_matrix_k.dot(mass_vector)
+    return p_k_plus_1, dpdt_k_plus_1
 
 def boundary_teleportation(p, sky_box_size):
     ### If the body left the preset region, teleport it to the other side of the boundary without changing the velocity vector
@@ -73,40 +121,3 @@ def boundary_teleportation(p, sky_box_size):
 
 
 
-
-
-
-
-
-
-
-
-# Euler's method
-def i_will_return_the_rough_n_body_problem_solution_for_im_using_the_eulers_method(dt, simulation_steps, initial_p, initial_dpdt, mass_vector):
-    # Log the initial conditions
-    time_stamp = np.ndarray(1)
-    time_stamp[0] = 0.0
-    p_solution = initial_p
-    dpdt_solution = initial_dpdt
-    
-    # Initial conditions & start calculating
-    p_k = initial_p
-    dpdt_k = initial_dpdt
-    for step in tqdm(range(simulation_steps)):
-        F_dir_matrix_k = update_gravtational_interaction_matrix(p_k) 
-        
-        # Euler's method 
-        p_k_plus_1 = p_k + dt * dpdt_k
-        dpdt_k_plus_1 = dpdt_k + dt * G * F_dir_matrix_k.dot(mass_vector)
-        # print(F_dir_matrix_k.dot(mass_vector))
-        # Log the results
-        time_stamp = np.c_[time_stamp, (step+1) * dt]
-        p_solution = np.c_[p_solution, p_k_plus_1]
-        dpdt_solution = np.c_[dpdt_solution, dpdt_k_plus_1]
-        
-        # Update p_[k] and dpdt_[k]
-        p_k = p_k_plus_1
-        dpdt_k = dpdt_k_plus_1
-        
-    solution = [time_stamp, p_solution, dpdt_solution]
-    return solution
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from aya_n_body_ultimate_calculator_pack.baby_math import *
-from aya_n_body_ultimate_calculator_pack.matplotlib_mp4_generator import *
+# from aya_n_body_ultimate_calculator_pack.matplotlib_mp4_generator import *
 from aya_n_body_ultimate_calculator_pack.n_body_class import *
-from aya_n_body_ultimate_calculator_pack.vpython_script_generator import *
+# from aya_n_body_ultimate_calculator_pack.vpython_script_generator import *
+
+
+three_body_system = n_body_system(5)
+
+three_body_system.generate_initial_conditions()
+three_body_system.engage_vpython_visualization(dt=100)
